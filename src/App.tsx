ece1e570f862ce91ff43c8968308e6993c8fab00
Gabriel <gabriel.bachelor@gmail.com>
2024-11-19 15:40:39 +0000

    compo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import Gods from "./gods.tsx";
+import { NavBarComponent } from "./components/hamburgerMenu/hamburger";
 function App() {
   const [] = useState(0)
-  return <>
+  return (
+  <>
+    <NavBarComponent></NavBarComponent>
     <Gods name="frigg"></Gods>
-  </>;
+  </>
+  )
 }
-
 export default App;
