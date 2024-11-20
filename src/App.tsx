5b474e059b4d5146c82ed75f486d3484c1b9f892
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-11-20 15:43:03 +0000

    #7 #8 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { NavBarComponent } from "./components/hamburgerMenu/hamburger";
-
-
-
-
-
-
-
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { MainLayout } from "./layouts/MainLayout";
+import { FrontPage } from "./pages/FrontPage.tsx";
 
 function App() {
-  return <>
-  <NavBarComponent></NavBarComponent>
-  
-  </>;
+  return (
+    <>
+      <HelmetProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path={"/"} element={<MainLayout />}>
+              <Route index element={<FrontPage />} />
+              <Route path={"/something"} element={<FrontPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
+    </>
+  );
 }
 
 export default App;
