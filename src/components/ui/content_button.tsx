793633c63c6d7cc7b2c3f9e2bbae74d1adc43425
Gabriel <gabriel.bachelor@gmail.com>
2024-11-21 10:57:44 +0000

    button

--- src/components/ui/content_button.tsx
+++ src/components/ui/content_button.tsx
@@ -5,7 +5,7 @@ function ContentButton() {
         className="bg-transparent text-gray-800 font-semibold py-2 px-10 border active:translate-x-1 active:translate-y-1 transition-transform duration-200
          border-black rounded shadow active:bg-black active:text-white font-montserrat"
       >
-        Button
+        
       </button>
     </>
   );
