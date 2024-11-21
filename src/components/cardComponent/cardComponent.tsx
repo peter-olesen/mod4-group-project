690e30b1d26f7d01182bf88790a8b494c2392576
crds83 <119341646+crds83@users.noreply.github.com>
2024-11-21 15:06:49 +0000

    odin text

--- src/components/cardComponent/cardComponent.tsx
+++ src/components/cardComponent/cardComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import odin from "../../assets/images/odin.jpg";
+import odin from "../../assets/images/formated images/Images/Odin.png";
 {
   /* odinData?.pageProps?.topic.image.url */
 }
@@ -7,6 +7,15 @@ import odin from "../../assets/images/odin.jpg";
 type OdinData = {
   pageProps?: {
     topic: {
+      summary: {
+        json: {
+          content: [
+            {
+              content: [{ value: string }];
+            }
+          ];
+        };
+      };
       title: string;
       image: {
         url: string;
@@ -35,18 +44,15 @@ export const CardComponent: React.FC<{}> = () => {
   }
   return (
     <div className=" rounded-lg shadow-lg overflow-hidden">
-      <div className="relative h-40 sm:h-48"></div>
+      {/*<div className="relative h-40 sm:h-48"></div>*/}
       <img src={odin} alt="" className="w-full object-cover object-center" />
       <div className="p-4">
-        <h2 className="text-xl font-bold text-gray-900 mb-2 sm:text-2xl">
+        <h2 className="text-xl text-center font-bold text-gray-900 mb-2 sm:text-2xl">
           {odinData?.pageProps?.topic.title}
         </h2>
       </div>
       <p className="text-gray-600 text-sm">
-        Known as the All-Father, Odin is the chief of the Aesir gods and ruler
-        of Asgard. He is associated with wisdom, war, death, and magic. Odin
-        sacrificed one of his eyes for knowledge and is often depicted with his
-        ravens, Huginn and Muninn.{" "}
+        {odinData?.pageProps?.topic.summary.json.content[0].content[0].value}
       </p>
     </div>
   );
