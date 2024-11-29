57249385572be478110658fd0b175d95e2aa0913
crds83 <119341646+crds83@users.noreply.github.com>
2024-11-29 11:19:02 +0000

    Merge branch 'dev' of https://github.com/peter-olesen/mod4-group-project into IcelandicSagas

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
     <>
       <NavBarComponent></NavBarComponent>
       {/* <IcelandicFolklore /> */}
-      <IcelandicFolklore />
+      <IcelandicSagas />
       <Footer></Footer>
 
       {/*  <Homepage />
