c34b9e2ed79ad34393a34b0485d130d44aab1980
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-12-02 13:21:41 +0000

    temp filenames

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ import { SectionLayout } from "./layouts/SectionLayout";
 import { QuizFrontpage } from "./pages/QuizFrontpage";
 import { QuizLayout } from "./layouts/QuizLayout";
 
-import { Homepage } from "./components/Homepage/Homepage";
+import { Homepage } from "./components/Homepage/temp_Homepage";
 import { NorseHomepage } from "./components/NorseHomepage/NorseHomepage";
 import { IcelandicFolkHome } from "./pages/IcelandicFolkHome";
 import { IcelandicSagas } from "./pages/IcelandicSagas";
 import { HistoricalMap } from "./pages/HistoricalMap";
 import { IcelandicFolklore } from "./pages/IcelandicFolklore";
 import { NorseExtended } from "./pages/NorseExtended";
-import { GodCardsPage } from "./pages/GodCardsPage";
+import { GodCardsPage } from "./pages/temp_GodCardsPage";
 
 const App: React.FC = () => {
   const [] = useState(0);
