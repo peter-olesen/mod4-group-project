33a6e6b0e361730116b4308276701d09751b789a
Peter Olesen <63056450+peter-olesen@users.noreply.github.com>
2024-12-02 10:37:50 +0000

    build

--- src/pages/Quiz1page.tsx
+++ src/pages/Quiz1page.tsx
@@ -1,81 +1,121 @@
-import React, { useState } from 'react';
-import backgroundImage from '../assets/images/QuizViking.jpg';
-import CircleTimer from '../CircleTimer/timer.tsx';
-import { QUIZ_QUESTIONS } from '@/assets/quizQuestions.ts';
+import { useState } from "react";
+import backgroundImage from "../assets/images/QuizViking.jpg";
+// import CircleTimer from '../CircleTimer/timer.tsx';
+import { QUIZ_QUESTIONS } from "@/assets/quizQuestions.ts";
 
 const Quiz1page = () => {
-    const [currQuestion,setCurrQuestion] = useState(0)
-    const [points,setPoints] = useState(0)
+  const [currQuestion, setCurrQuestion] = useState(0);
+  const [points, setPoints] = useState(0);
 
-    // Quiz is finished. Start over
-  if(currQuestion > QUIZ_QUESTIONS.length-1) return (
-  <><div
-  className="w-full max-w-[90%] sm:max-w-[70%] flex flex-col justify-center items-center h-screen bg-cover bg-center gap-4 sm:gap-2 md:gap-6"
-  >
-    {`YOU´RE FINISH. CONGRATULATIONS! ${points} points`} <img src={backgroundImage}/> </div><button onClick={()=>{setPoints(0);setCurrQuestion(0)}}>To start again</button></>)
-  
-    // update place in the quiz and the score
-  const handleOnClick = (isAnswer:boolean) => {
-      if(isAnswer) setPoints((prev)=>prev+10)
-      setCurrQuestion((prev)=>prev+1)
-    }
-    
-
-    // Create an Option component to house the possible answers and know if they are true
-  const Option = ({optionString,isAnswer,abc}:{optionString:string,isAnswer:boolean,abc:"A"|"B"|"C"}) => {
-
+  // Quiz is finished. Start over
+  if (currQuestion > QUIZ_QUESTIONS.length - 1)
     return (
-    <div className="w-full max-w-[90%] sm:max-w-[70%] bg-gray-300 text-black p-2 rounded-lg font-[Montserrat] text-left pb-[12px] pl-[21px] text-[clamp(14px, 2vw, 16px)]" onClick={()=>handleOnClick(isAnswer)}>
-  {`${abc}. ${optionString}`}
-  </div>)
-  }
+      <>
+        <div className="w-full max-w-[90%] sm:max-w-[70%] flex flex-col justify-center items-center h-screen bg-cover bg-center gap-4 sm:gap-2 md:gap-6">
+          {`YOU´RE FINISH. CONGRATULATIONS! ${points} points`}{" "}
+          <img src={backgroundImage} />{" "}
+        </div>
+        <button
+          onClick={() => {
+            setPoints(0);
+            setCurrQuestion(0);
+          }}
+        >
+          To start again
+        </button>
+      </>
+    );
+
+  // update place in the quiz and the score
+  const handleOnClick = (isAnswer: boolean) => {
+    if (isAnswer) setPoints((prev) => prev + 10);
+    setCurrQuestion((prev) => prev + 1);
+  };
+
+  // Create an Option component to house the possible answers and know if they are true
+  const Option = ({
+    optionString,
+    isAnswer,
+    abc,
+  }: {
+    optionString: string;
+    isAnswer: boolean;
+    abc: "A" | "B" | "C";
+  }) => {
+    return (
+      <div
+        className="w-full max-w-[90%] sm:max-w-[70%] bg-gray-300 text-black p-2 rounded-lg font-[Montserrat] text-left pb-[12px] pl-[21px] text-[clamp(14px, 2vw, 16px)]"
+        onClick={() => handleOnClick(isAnswer)}
+      >
+        {`${abc}. ${optionString}`}
+      </div>
+    );
+  };
 
   // Randomise the options
-  const {options} = QUIZ_QUESTIONS[currQuestion]
+  const { options } = QUIZ_QUESTIONS[currQuestion];
 
-  const newOptions = new Array(options.length)
-  for (let i=0;i<options.length;i++) {
-    while(true) {
-      const num = Math.floor(Math.random() * options.length) 
-      if(i===0) {newOptions[num] = {string:options[i],isAnswer:true};break;}
-      if(!newOptions[num]) {newOptions[num] = {string:options[i],isAnswer:false};break;}
+  const newOptions = new Array(options.length);
+  for (let i = 0; i < options.length; i++) {
+    while (true) {
+      const num = Math.floor(Math.random() * options.length);
+      if (i === 0) {
+        newOptions[num] = { string: options[i], isAnswer: true };
+        break;
+      }
+      if (!newOptions[num]) {
+        newOptions[num] = { string: options[i], isAnswer: false };
+        break;
+      }
     }
   }
 
-  console.log(newOptions)
+  console.log(newOptions);
 
   return (
-        <div 
-     className="w-full max-w-[90%] sm:max-w-[70%] flex flex-col justify-center items-center h-screen bg-cover bg-center gap-4 sm:gap-2 md:gap-6 border border-white"
-        style={{ backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
+    <div
+      className="w-full max-w-[90%] sm:max-w-[70%] flex flex-col justify-center items-center h-screen bg-cover bg-center gap-4 sm:gap-2 md:gap-6 border border-white"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
       }} // Image background
-      >
+    >
       {/* <div className="text-[clamp(16px, 4vw, 24px)] text-right w-full pr-4">
         X
       </div> */}
-      <div className='w-full max-w-[90%] sm:max-w-[70%] text-[20px] text-right pr-8 mb-48 mt-32'>X 
+      <div className="w-full max-w-[90%] sm:max-w-[70%] text-[20px] text-right pr-8 mb-48 mt-32">
+        X
       </div>
 
-      <div className="w-full max-w-[90%] sm:max-w-[70%] border border-black h-[6px] -mb-4 bg-white"><br></br>
+      <div className="w-full max-w-[90%] sm:max-w-[70%] border border-black h-[6px] -mb-4 bg-white">
+        <br></br>
       </div>
-    
-      <div className="bg-gray-300 text-black p-2 rounded-lg font-[Montserrat] text-left pb-[12px] pl-[21px] text-[16px] w-full max-w-[90%] sm:max-w-[70%]" >
+
+      <div className="bg-gray-300 text-black p-2 rounded-lg font-[Montserrat] text-left pb-[12px] pl-[21px] text-[16px] w-full max-w-[90%] sm:max-w-[70%]">
         {QUIZ_QUESTIONS[currQuestion].question}
       </div>
 
-  {newOptions.map((option,index)=>{ // Map over the newOptions array and generate the JSX
-  const {string,isAnswer} = option
-  const abc:"A"|"B"|"C" = index === 0 ? "A" : index === 1 ? "B" : "C"
+      {newOptions.map((option, index) => {
+        // Map over the newOptions array and generate the JSX
+        const { string, isAnswer } = option;
+        const abc: "A" | "B" | "C" =
+          index === 0 ? "A" : index === 1 ? "B" : "C";
 
-  return(<Option optionString={string} isAnswer={isAnswer} abc={abc} key={index}/>)
-  })}
+        return (
+          <Option
+            optionString={string}
+            isAnswer={isAnswer}
+            abc={abc}
+            key={index}
+          />
+        );
+      })}
 
       <div className="w-full max-w-[90%] sm:max-w-[70%] bg-gray-300 text-black p-2 rounded-lg font-[Montserrat] pb-[12px] pt-[8px] text-[clamp(14px, 2vw, 16px) text-center mb-[360px] mt-16">
-      {`${points} points`}
+        {`${points} points`}
       </div>
     </div>
   );
-}
+};
 
 export default Quiz1page;
